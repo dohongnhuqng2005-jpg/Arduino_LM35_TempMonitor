674bf0137a7a79618f7a90b3ed60962547e84897
dohongnhuqng2005@gmail.com <dohongnhuqng2005@gmail.com>
2026-04-14 17:48:46 +0700

    feat: ho tro doc 2 kenh LM35 (A0, A1)

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -3,17 +3,26 @@
  * Mon: Mang Cam Bien - Tuan 03
  */
 
-int adcValue;
-float nhietDo;
+int adcValues[2]; 
+float nhietDo[2]; 
+char chuoi[30];
 
 void setup() {
   Serial.begin(9600);
-  delay(100);
 }
 
 void loop() {
-  adcValue = analogRead(A0);
-  nhietDo = (adcValue * 500.0) / 1023.0;
-  Serial.println(nhietDo, 1);
-  delay(1000);
+  // Đọc kênh 1 (A0)
+  adcValues[0] = analogRead(A0);
+  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+
+  // Đọc kênh 2 (A1)
+  adcValues[1] = analogRead(A1);
+  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+
+  // Chuyển dữ liệu sang dạng CSV để gửi qua Serial
+  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+  Serial.print(chuoi);
+
+  delay(100);
 }
